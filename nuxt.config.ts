60ddf484dac687cab617d7f8865d4164e07253ba
VINXIS <oykxf2@gmail.com>
2019-12-18 02:42:51 -0700

    get host/port from config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
+import { Config } from "../config"
+const config = new Config;
 export default {
   serverMiddleware: ['~/api'],
   buildModules: ['@nuxt/typescript-build'],
+  server: {
+    host: config.ayim.host,
+    port: config.ayim.port,
+  },
 }
