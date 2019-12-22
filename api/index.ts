b1b4d8a61bcd2ff7d39154c1436741674a13988a
VINXIS <oykxf2@gmail.com>
2019-12-21 21:51:39 -0700

    keys

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import { Config } from "../../config"
 import { App } from "../../commonRoutes"
 
 const config = new Config()
-const app = new App(config.ayim.publicURL)
+const app = new App(config.ayim.publicURL, config.ayim.keys)
 
 export default {
     path: "/api",
