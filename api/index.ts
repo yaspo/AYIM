47986439c3f705512af88c865567d0064e427ba4
yaspo <yasporubenvl@gmail.com>
2020-07-06 00:35:55 +0200

    ayim frontpage finished

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,14 @@
 import "reflect-metadata";
 import { App } from "../../CorsaceServer";
 import mount from "koa-mount";
+import UserRouter from "./routes/user";
 import commentsRouter from "./routes/comments";
 import commentsReviewRouter from "./routes/staff/comments";
 import usersRouter from "./routes/staff/users";
 
 const app = new App("ayim");
 
+app.koa.use(mount("/user", UserRouter.routes()));
 app.koa.use(mount("/comments", commentsRouter.routes()));
 app.koa.use(mount("/staff/comments", commentsReviewRouter.routes()));
 app.koa.use(mount("/staff/users", usersRouter.routes()));
--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,36 @@
 <template>
-    <div>
-        <a
-            v-if="!user || user.discord.userID === ''"
-            href="/api/login/discord"
-        >login discord</a>
-        <a
-            v-if="(user && user.osu.userID === '')"
-            href="/api/login/osu"
-        > login osu!</a>
-        <div v-if="user">
-            Discord username: {{ user.discord.username }}<br>
-            osu! username: {{ user.osu.username }}<br>
-            <p v-if="eligible">
-                You are eligible
-            </p>
-            <p v-else>
-                You are not eligible
-            </p>
-        </div>
-        {{ value }}
-        <div @click="run">
-            click to run
-        </div>
+    <div class="home">
+        <div class="left-side"></div>
+        <div class="right-side">
+           
+            <modeSwitcher 
+                :page="'index'"
+            />
+         </div>
     </div>
 </template>
 
 <script>
 import axios from "axios";
 
+import modeSwitcher from "../components/mode/modeSwitcher";
+
 export default {
+    components: {
+        "modeSwitcher": modeSwitcher,
+    },
     data () {
         return {
             user: null,
             value: "0%",
             eligible: false,
+            selectedMode: "",
+            modes: ["standard", "taiko", "fruits", "mania", "storyboard"],
         };
     },
     mounted: async function() {
+        this.selectedMode = this.$route.params.mode || "standard";
+
         try {
             const data = (await axios.get(`/api/user`)).data;
             
@@ -56,3 +50,166 @@ export default {
     },
 };
 </script>
+
+<style lang="scss">
+$modes: "storyboard", "mania" , "fruits", "taiko", "standard";
+
+.home {
+    width: 100%;
+    padding-bottom: 9vh;
+   
+    display: flex;
+    flex-grow: 1;
+        
+    @media (min-width: 1200px) {
+        margin-bottom: 0px;
+    }
+
+    background-image: url("../../CorsaceAssets/img/ayim-mca/site/ayim bg.png");
+    background-size: auto 80%;
+    background-repeat: no-repeat;
+
+
+}
+
+.right-side {
+    display: flex;
+    height: 100%;
+    align-items: flex-end;
+}
+
+
+
+
+@mixin mode-container {
+    @each $mode in $modes {
+        &--#{$mode} {
+            border-top: 3px solid var(--#{$mode});
+
+            &::before {
+                border-left: 3px solid var(--#{$mode});
+            }
+        }
+    }
+}
+
+@mixin mode-vote-color {
+    @each $mode in $modes {
+        &--#{$mode} {
+            color: var(--#{$mode});
+            background: linear-gradient(135deg,#222 0%, #222 20%, white 20%, white 22%, #222 22%, #222 24%, white 24%, white 26%, var(--#{$mode}) 26%, var(--#{$mode}) 28%, white 28%);
+        }
+    }
+}
+
+@mixin mode-selection-border {
+    @each $mode in $modes {
+        $i: index($modes, $mode);
+
+        &--#{$mode} {
+            &::before {
+                border-bottom: 3px solid var(--#{$mode});
+                // - icon size + border margin - icon margin
+                width: calc(100% - 45px * #{$i} + 28px - 15px * #{$i - 1});
+            }
+        }
+
+        &__mode {
+            &--#{$mode} {
+                background-image: url("../../CorsaceAssets/img/ayim-mca/#{$mode}.png");
+
+                &::before {
+                    border-bottom: 3px solid var(--#{$mode});
+                }
+            }
+
+            &--#{$mode}-selected {
+                background-color: var(--#{$mode});
+            }
+        }
+    }
+}
+
+%spaced-container {
+    margin-bottom: 40px;
+    display: flex;
+    justify-content: space-around;
+}
+
+.mode-selection {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    position: relative;
+    @include mode-selection-border;
+
+    &__mode {
+        width: 45px;
+        height: 45px;
+        background-repeat: no-repeat;
+        background-size: cover;
+        border-radius: 100%;
+        margin-left: 15px;
+    }
+
+    &::before {
+        content: "";
+        position: absolute;
+        bottom: 50%;
+        left: -28px;
+        z-index: -1;
+        border-bottom-left-radius: 25px;
+        height: 100%;
+    }
+
+}
+
+.mode-container {
+    @include mode-container;
+
+    border-top-left-radius: 25px;
+    padding: 25px;
+    position: relative;
+
+    &::before {
+        content: "";
+        position: absolute;
+        top: -2px;
+        left: -3px;
+        border-bottom-left-radius: 25px;
+        border-top-left-radius: 25px;
+        width: 100%;
+        height: calc(100% - 45px);
+        z-index: -1;
+    }
+
+    &__general {
+        @extend %spaced-container;
+        flex-wrap: wrap;
+
+        @media (min-width: 1200px) {
+            flex-wrap: nowrap;
+        }
+    }
+
+    &__stats {
+        margin-bottom: 20px;
+
+        @media (min-width: 1200px) {
+            flex-wrap: nowrap;
+        }
+    }
+}
+
+@media (min-width: 992px) {    
+    .left-side {
+        flex: 0 0 40%;
+        max-width: 40%;
+    }
+    .right-side {
+        flex: 0 0 60%;
+        max-width: 60%;
+    }
+}
+
+</style>
